14d8c7e7ae07184e2b007e1984a4314832dcb09a
Chui Tey <teyc@cognoware.com>
2019-11-06 20:57:56 +1000

    Better demonstrate protected sections

--- Gsl.Tests/data/Customer.cs
+++ Gsl.Tests/data/Customer.cs
@@ -6,19 +6,19 @@
         {
             get
             {
-                // CUSTOM-CODE-BEGIN get-FirstName
+                /* CUSTOM-CODE-BEGIN get-FirstName */
                 /* 12-May-2018 Note there there are no validation rules in the DB */
                 return _firstName;
-                // CUSTOM-CODE-END get-FirstName
+                /* CUSTOM-CODE-END get-FirstName */
             }
         }
         public string LastName
         {
             get
             {
-                // CUSTOM-CODE-BEGIN get-LastName
+                /* CUSTOM-CODE-BEGIN get-LastName */
                 return _lastName;
-                // CUSTOM-CODE-END get-LastName
+                /* CUSTOM-CODE-END get-LastName */
             }
         }
     }
