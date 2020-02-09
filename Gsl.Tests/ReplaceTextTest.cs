c84d0a595b6be3c6bd133d91ffe72e0435c1cc14
Chui Tey <teyc@cognoware.com>
2020-02-10 07:21:33 +1000

    Built failing test

---
+++ Gsl.Tests/ReplaceTextTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+
+namespace Gsl.Tests
+{
+    public class ReplaceTextTest : TestBase
+    {
+        public ReplaceTextTest(Xunit.Abstractions.ITestOutputHelper log) : base(log)
+        {
+        }
+
+        /// <summary>
+        /// All raw text are macro expanded when replaceText definition is found
+        /// </summary>
+        [Fact]
+        public void TemplateTextShouldBeReplaced()
+        {
+            const string template = "data/replaceText.gsl";
+            const string data = "data/replaceText.json";
+
+            TemplateWithSingleFileOutput(template, data);
+        }
+    }
+}
