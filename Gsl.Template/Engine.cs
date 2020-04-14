190144ffa7bee1354cc91cdf8d999a8bd15d8370
Chui Tey <teyc@cognoware.com>
2020-04-14 21:47:56 +1000

    Execute templating engine with a dictionary

--- Gsl.Template/Engine.cs
+++ Gsl.Template/Engine.cs
@@ -1,6 +1,7 @@
 using Jint.Native.Json;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.IO.Abstractions;
 
 namespace Gsl
@@ -36,7 +37,10 @@ namespace Gsl
 
         private IFileInfo[] Execute(string template, string dataContents, string templatePath)
         {
-            var jsEngine = new Jint.Engine(options => options.DebugMode())
+            var jsEngine = new Jint.Engine(options => {
+                    options.AllowClr();
+                    options.DebugMode();
+                })
               .SetValue("__expandText", new Func<string, string>(vm.ExpandText))
               .SetValue("replaceText", new Action<string, string>(vm.ReplaceText))
               .SetValue("log", new Action<object>(line => logger.LogInformation("log: " + line)))
@@ -56,6 +60,41 @@ namespace Gsl
             foreach (var property in data.AsObject().GetOwnProperties())
             {
                 jsEngine.Global.Put(property.Key, property.Value.Value, true);
+            }
+
+            vm.EvaluateTemplate(jsEngine: jsEngine, templatePath: templatePath, templateContent: template);
+
+            return vm.GetOutputFiles();
+        }
+
+        public IFileInfo[] Execute(string template, IDictionary<string,object> data, string templatePath)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+
+            var jsEngine = new Jint.Engine(options =>
+            {
+                options.AllowClr();
+                options.DebugMode();
+            })
+              .SetValue("__expandText", new Func<string, string>(vm.ExpandText))
+              .SetValue("replaceText", new Action<string, string>(vm.ReplaceText))
+              .SetValue("log", new Action<object>(line => logger.LogInformation("log: " + line)))
+              .SetValue("kebabCase", new Func<string, string>(StringFunctions.KebabCase))
+              .SetValue("camelCase", new Func<string, string>(StringFunctions.CamelCase))
+              .SetValue("output", new Action<object>(vm.WriteLine))
+              .SetValue("outputAligned", new Action<int, string>(vm.WriteLineAligned))
+              .SetValue("protect", new Action<string, string, string>(vm.WriteProtectedSection))
+              .SetValue("doNotOverwriteIf", new Action<string, string>(vm.DoNotOverwriteIf))
+              .SetValue("setOutput", new Action<string>(vm.SetOutput));
+
+            jsEngine
+              .SetValue("include", new Action<string>(
+                  relativePath => vm.EvaluateTemplate(jsEngine: jsEngine, templatePath: relativePath)));
+
+            foreach (var property in data)
+            {
+                jsEngine.SetValue(property.Key, property.Value);
             }
 
             vm.EvaluateTemplate(jsEngine: jsEngine, templatePath: templatePath, templateContent: template);
